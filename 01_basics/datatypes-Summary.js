e8965f78f7d8d3cf3c4eebb9c2603d7bb26ffe6a
PRIYANSHU1291 <116941784+PRIYANSHU1291@users.noreply.github.com>
2025-05-03 13:13:01 +0000

    summary

--- 01_basics/datatypes-Summary.js
+++ 01_basics/datatypes-Summary.js
@@ -11,4 +11,33 @@ const myFunction = function(){
     console.log("hello world");
 }
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myYoutubeChannel = "one_min_updates"
+
+let anothername = myYoutubeChannel
+anothername = "anime"
+
+console.log(anothername);
+console.log(myYoutubeChannel);
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo =userOne
+
+userTwo.email = "google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
