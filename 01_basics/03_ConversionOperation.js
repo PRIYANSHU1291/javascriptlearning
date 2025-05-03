a6c0c6af3dfb6e17db8c5c95c570a2220ee2ed94
PRIYANSHU1291 <116941784+PRIYANSHU1291@users.noreply.github.com>
2025-05-03 06:27:35 +0000

    ConversionOperations

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -46,7 +46,45 @@ console.log(booleanIsLoggedIn);
 // "" => false
 // "priyanshu" => true
 
+/*
 let someNumber = 33
 
 let StringNumber = String(someNumber)
 console.log(typeof StringNumber)
+*/
+
+// ************************* Operations ****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/32);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "priyanshu"
+let str3 = str1 + str2
+// console.log(str3); // hellopriyanshu 
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(true) //true
+// console.log(+true) // 1
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter); //101
+
+
