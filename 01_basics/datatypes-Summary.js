fa94ba08577b6686ed129c1a3d1c6cb279c97482
PRIYANSHU1291 <116941784+PRIYANSHU1291@users.noreply.github.com>
2025-05-03 12:55:20 +0000

    change in location of file

---
+++ 01_basics/datatypes-Summary.js
@@ -0,0 +1,14 @@
+const bigNumber = 3456543576654356754n
+
+const heros = ["shaktiman","naagraj","doga"];
+
+let myObj = {
+    name: "priyanshu",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof myFunction);
