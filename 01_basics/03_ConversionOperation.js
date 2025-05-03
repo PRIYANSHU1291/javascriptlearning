add7bed6f798f8dad556d785fb9b50027d004ec4
PRIYANSHU1291 <116941784+PRIYANSHU1291@users.noreply.github.com>
2025-05-03 05:46:52 +0000

    ConversionOperation

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,52 @@
+/*
+let score = "33abcc"
+
+console.log(typeof score); //string
+console.log(typeof (score)); //string
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber) // NaN
+*/
+
+/*
+let score = null
+
+console.log(typeof score); //object
+console.log(typeof (score)); //object
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber) // 0
+*/
+
+/*
+let score = undefined
+
+console.log(typeof score); //undefined
+console.log(typeof (score)); //undefined
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber) // NaN
+*/
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+/*
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+*/
+
+// 1 => true; 0 => false
+// "" => false
+// "priyanshu" => true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(typeof StringNumber)
